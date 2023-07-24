dd58e2af7eeb54e138c622931120859c9ccabab9
MartinLRead <124619850+MartinLRead@users.noreply.github.com>
2023-07-24 15:47:10 +0100

    Updates to various files

--- Read_Martin_DA301_Assignment_R_Workbook.R
+++ Read_Martin_DA301_Assignment_R_Workbook.R
@@ -54,8 +54,9 @@
 
 # Install and import Tidyverse.
 library('tidyverse')
+library(dplyr)
 
-# Import the data set.
+# Import the data set. Located in 'Data' subfolder in Github.
 turtlesales <- read.csv(file.choose(), header=T)
 
 # Print the data frame.
@@ -67,6 +68,7 @@ sales2 <- select(turtlesales, -Ranking, -Year, -Genre, -Publisher)
 sales2$Product_factor <- factor(sales2$Product)
 # View the data frame.
 head(sales2)
+glimpse(sales2)
 
 column_names <- colnames(sales2)
 print(column_names)
@@ -100,13 +102,16 @@ qplot(GroupedProduct_factor, data = sales2, geom = 'bar') +
   ylab("Count")
 
 grouped_sales <- aggregate(Global_Sales ~ Platform, data = sales2, sum)
-ggplot(grouped_sales, aes(x = Platform, y = Global_Sales)) +
+plot1 <- ggplot(grouped_sales, aes(x = Platform, y = Global_Sales)) +
   geom_bar(stat = "identity") +
   ylab("Global Sales")
 
+ggplotly(plot1)
+
 ## 2c) Boxplots
 # Create boxplots.
-qplot(GroupedProduct_factor,Global_Sales, data=sales2, colour=I('red'), geom='boxplot')
+plot2 <- qplot(GroupedProduct_factor,Global_Sales, data=sales2, colour=I('red'), geom='boxplot')
+ggplotly(plot2)
 
 ###############################################################################
 
@@ -120,13 +125,15 @@ qplot(GroupedProduct_factor,Global_Sales, data=sales2, colour=I('red'), geom='bo
 sales3 <- arrange(sales2, desc(Global_Sales))
 head(sales3)
 
+sapply(sales2$Global_Sales, mean)
+
 # As being product code 107.
 
 ###############################################################################
 ###############################################################################
 
 
-# Week 5 assignment: Cleaning and maniulating data using R
+# Week 5 assignment: Cleaning and manipulating data using R
 
 ## Utilising R, you will explore, prepare and explain the normality of the data
 ## set based on plots, Skewness, Kurtosis, and a Shapiro-Wilk test. Note that
@@ -164,13 +171,14 @@ head(sales3)
 # 1. Load and explore the data
 
 # View data frame created in Week 4.
-
+head(sales2)
+glimpse(sales2)
 
 # Check output: Determine the min, max, and mean values.
-
+summary(sales2)
 
 # View the descriptive statistics.
-
+DataExplorer::create_report(sales2)
 
 ###############################################################################
 
@@ -178,24 +186,55 @@ head(sales3)
 
 ## 2a) Use the group_by and aggregate functions.
 # Group data based on Product and determine the sum per Product.
-
+df_sales <- sales2 %>% group_by(Product_factor) %>%
+  summarise(EU_Sales=sum(EU_Sales),
+            NA_Sales=sum(NA_Sales),
+            Global_Sales=sum(Global_Sales),
+            .groups='drop')
 
 # View the data frame.
-
+df_sales
 
 # Explore the data frame.
-
+write.csv(df_sales, file = "C:/Users/marti/OneDrive/Documents/GitHub/Read_Martin_DA301_Assignment/df_sales.csv", row.names = FALSE)
 
 
 ## 2b) Determine which plot is the best to compare game sales.
 # Create scatterplots.
 
+ggplot(data = df_sales,
+       mapping = aes(x = Product_factor, y = Global_Sales)) +
+  geom_point(color = 'red', alpha = 0.5, size = 1.5) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
 # Create histograms.
+ggplot(sales3, aes(x = Product_factor, y = sum(Global_Sales))) +
+geom_bar(stat = "summary", fun = "sum") +
+labs(x = "Product", y = "Sum of Global Sales", title = "Sum of Global Sales by Product")
 
+# Also:
+ggplot(sales3, aes(x=NA_Sales)) + 
+geom_histogram(bins = 20) +
 
-# Create boxplots.
+ggplot(sales3, aes(x=EU_Sales)) + 
+geom_histogram(bins = 20)
 
+# Boxplots with notch for median
+ggplot(df_sales, aes(x = Global_Sales, y = Global_Sales)) +
+geom_boxplot(fill = 'red', notch = TRUE, outlier.color = 'red') +
+labs(title = "Box plot for Global Sales") +  
+theme_minimal() 
+
+# Boxplot with outlier labels
+p <- ggplot(df_sales, aes(x = "", y = Global_Sales)) +
+  geom_boxplot() +
+  labs(x = NULL, y = "Global Sales", title = "Boxplot with Outlier Labels")
+# Calculate the boxplot stats to identify outliers
+boxplot_stats <- boxplot.stats(df_sales$Global_Sales)
+# Identify outliers and their corresponding product numbers
+outliers <- df_sales[df_sales$Global_Sales %in% boxplot_stats$out, ]
+# Add labels to the boxplot for outliers
+p + geom_text(data = outliers, aes(label = Product_factor), vjust = -0.5)
 
 ###############################################################################
 
@@ -203,26 +242,33 @@ head(sales3)
 # 3. Determine the normality of the data set.
 
 ## 3a) Create Q-Q Plots
-# Create Q-Q Plots.
 
-
+qqnorm(df_sales$NA_Sales)
+# Add a reference line:
+qqline(df_sales$NA_Sales, col='red')
 
 ## 3b) Perform Shapiro-Wilk test
 # Install and import Moments.
-
+library(moments)
 
 # Perform Shapiro-Wilk test.
-
-
+shapiro.test(df_sales$Global_Sales)
+shapiro.test(df_sales$NA_Sales)
+shapiro.test(df_sales$EU_Sales)
+# p-value of this test is less than 0.05 --> Not normal
 
 ## 3c) Determine Skewness and Kurtosis
-# Skewness and Kurtosis.
 
+skewness(df_sales$Global_Sales) 
+skewness(df_sales$NA_Sales) 
+skewness(df_sales$EU_Sales) 
+# skewness falls outside the range of -0.5 and 0.5
 
+kurtosis(df_sales$Global_Sales)
 
 ## 3d) Determine correlation
 # Determine correlation.
-
+cor(df_sales$EU_Sales, df_sales$NA_Sales)
 
 ###############################################################################
 
@@ -231,12 +277,33 @@ head(sales3)
 # Choose the type of plot you think best suits the data set and what you want 
 # to investigate. Explain your answer in your report.
 
+# Summarize the data by Platform
+sales_summary <- sales2 %>%
+  group_by(Platform) %>%
+  summarize(Total_EU_Sales = sum(EU_Sales), Total_NA_Sales = sum(NA_Sales))
+
+# Reshape the data into a longer format
+sales_summary_long <- sales_summary %>%
+  pivot_longer(cols = c(Total_EU_Sales, Total_NA_Sales),
+               names_to = "Region",
+               values_to = "Total_Sales")
+
+# Create the bar plot
+ggplot(sales_summary_long, aes(x = Platform, y = Total_Sales, fill = Region)) +
+  geom_bar(position = "dodge", stat = "identity") +
+  scale_fill_manual(values = c('blue', 'darkgreen'),
+                    name = "Region",
+                    labels = c("EU Sales", "NA Sales")) + 
+labs(x = "Platform", y = "Total Sales")
+
 
 ###############################################################################
 
 # 5. Observations and insights
 # Your observations and insights here...
 
+# Wii and XBox360 sales much higher in NA than EU.
+# NA sales higher than EU for every platform except PC and PS3.
 
 
 ###############################################################################
@@ -278,12 +345,12 @@ head(sales3)
 
 ###############################################################################
 
-# 1. Load and explor the data
+# 1. Load and explore the data
 # View data frame created in Week 5.
-
+head(sales2)
 
 # Determine a summary of the data frame.
-
+summary(sales2)
 
 ###############################################################################
 
@@ -291,10 +358,34 @@ head(sales3)
 ## 2a) Determine the correlation between columns
 # Create a linear regression model on the original data.
 
+model1 <- lm(Global_Sales~NA_Sales,
+             data=sales2)
+
+# View the model.
+view(model1)
+
+# View more outputs for the model - the full regression table.
+summary(model1)
 
 
 ## 2b) Create a plot (simple linear regression)
 # Basic visualisation.
+salesplot1 <- plot(model1)
+salesplot1 
+
+
+## 2c) Filter the outlier rows
+
+rows_to_drop <- c(1, 2, 6, 10)
+sales2_filtered <- sales2[-rows_to_drop, ]
+
+model2 <- lm(Global_Sales~NA_Sales,
+             data=sales2_filtered)
+
+view(model2)
+salesplot2 
+
+summary(model2)
 
 
 ###############################################################################
@@ -306,11 +397,55 @@ head(sales3)
 # Multiple linear regression model.
 
 
+model3 = lm(Global_Sales~NA_Sales+EU_Sales,
+            data=sales2)
+
+# Change the model name and plot it
+summary(model3)
+
+salesplot3<- plot(model3)
+salesplot3
+
+
+# See whether a log transformation of EU Sales (with outliers) gives a better R2
+
+library(dplyr)
+logEU_Sales <- mutate(sales2, 
+              logEU_Sales =log(EU_Sales))
+
+
+# View new object with new variable.
+head(logEU_Sales)
+# Remove rows with na or infinite values:
+logEU_Sales <- na.omit(logEU_Sales) 
+logEU_Sales <- logEU_Sales[is.finite(logEU_Sales$logEU_Sales), ]
+
+# Create a new model using logEU_Sales
+model4 <- lm(Global_Sales~logEU_Sales,
+             data=logEU_Sales)
+summary(model4)
+
+
+# Plot model2:
+plot((sales2$NA_Sales + sales2$EU_Sales), sales2$Global_Sales)
+# Add a line-of-best fit to existing plot.
+abline(coefficients(model2))
+
+
 ###############################################################################
 
 # 4. Predictions based on given values
-# Compare with observed values for a number of records.
+# Compare with observed values for a number of records (test data).
 
+# Load the new data file (model2_test.csv) (Test data was created in Excel)
+model2test <- read.csv(file.choose(), header=TRUE)
+head(model2test)
+
+# Create a new object and specify the predict function.
+predictTest = predict(model2, newdata=model2test,
+                      interval='confidence')
+# Print the object.
+predictTest 
 
 
 ###############################################################################
